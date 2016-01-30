37238e33e7201c8108a045c9caf1a0ccd6a15453
Michael Neumann <mneumann@ntecs.de>
2016-01-30 17:22:31 +0100

    Implement offspring generation half-way

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(iter_arith)]
+
 extern crate rand;
 
 use std::collections::BTreeMap;
@@ -6,17 +8,20 @@ use innovation::Innovation;
 use rand::{Rng, Closed01};
 
 mod innovation;
+mod selection;
 
 trait Gene: Clone {
     fn weight_distance(&self, other: &Self) -> f64;
 }
 
+#[derive(Copy, Clone)]
 enum NodeType {
     Input,
     Output,
     Hidden,
 }
 
+#[derive(Clone)]
 struct NodeGene {
     node_type: NodeType,
 }
@@ -39,6 +44,7 @@ impl Gene for LinkGene {
     }
 }
 
+#[derive(Clone)]
 struct Genome {
     link_genes: BTreeMap<Innovation, LinkGene>,
     node_genes: BTreeMap<Innovation, NodeGene>,
@@ -52,7 +58,7 @@ enum Correlation<'a, T: 'a> {
     ExcessRight(&'a T),
 }
 
-/// Correlate the genes in ```genes_left``` with those in ```genes_right```.
+/// Correlate the genes in `genes_left` with those in `genes_right`.
 fn correlate<'a, T, F>(genes_left: &'a BTreeMap<Innovation, T>,
                        genes_right: &'a BTreeMap<Innovation, T>,
                        f: &mut F)
@@ -154,14 +160,15 @@ struct CrossoverProbabilities {
 fn is_probable<R: Rng>(prob: &Closed01<f32>, rng: &mut R) -> bool {
     if prob.0 < 1.0 {
         let v: f32 = rng.gen(); // half open [0, 1)
+        debug_assert!(v >= 0.0 && v < 1.0);
         v < prob.0
     } else {
         true
     }
 }
 
-/// We assume ```parent1``` to be the fitter parent. Takes gene 
-/// either from ```parent1``` or ```parent2``` according to
+/// We assume `parent1` to be the fitter parent. Takes gene 
+/// either from `parent1` or `parent2` according to
 /// the probabilities specified and the relative fitness of the parents.
 fn crossover<T: Clone, R: Rng>(parent1: &BTreeMap<Innovation, T>,
                                parent2: &BTreeMap<Innovation, T>,
@@ -221,7 +228,19 @@ struct Niche {
 }
 
 impl Niche {
+    fn len(&self) -> usize {
+        let l = self.genomes.len();
+        assert!(l > 0);
+        l
+    }
+
+    // Return true if genome at position `i` is better that `j`
+    fn compare_ij(&self, i: usize, j: usize) -> bool {
+        self.genomes[i].0 > self.genomes[j].0
+    }
+
     fn new_with(fitness: Fitness, genome: Box<Genome>) -> Niche {
+        assert!(fitness >= 0.0);
         Niche {
             genomes: vec![(fitness, genome)],
             fitness_sum: fitness,
@@ -229,14 +248,18 @@ impl Niche {
     }
 
     fn mean_fitness(&self) -> Fitness {
-        assert!(self.genomes.len() > 0);
-        self.fitness_sum / (self.genomes.len() as Fitness)
+        self.fitness_sum / (self.len() as Fitness)
     }
 
     fn add(&mut self, fitness: Fitness, genome: Box<Genome>) {
-        assert!(self.genomes.len() > 0);
+        assert!(fitness >= 0.0);
         self.genomes.push((fitness, genome));
         self.fitness_sum += fitness;
+    }
+
+    fn sort(mut self) -> Self {
+        (&mut self.genomes).sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(cmp::Ordering::Less));
+        self
     }
 }
 
@@ -245,6 +268,13 @@ struct Population {
 }
 
 impl Population {
+    fn len(&self) -> usize {
+        self.genomes.len()
+    }
+
+    // We want to create a population of approximately ```pop_size```.
+    // We do not care if there are slightly more individuals than that.
+    //
     // 1. sort whole popluation into niches
     // 2. calculate the number of offspring for each niche.
     // 3. each niche produces offspring.
@@ -252,14 +282,85 @@ impl Population {
     //    - determine the elitist size. those are always copied into the new generation.
     //    - r% of the best genomes produce offspring
     //
-    fn produce_offspring(self) {}
+    fn produce_offspring<R>(self,
+                            pop_size: usize,
+                            // how many of the best individuals of a niche are copied as-is into the
+                            // new population?
+                            elite_percentage: Closed01<f64>,
+                            // how many of the best individuals of a niche are selected for
+                            // reproduction?
+                            selection_percentage: Closed01<f64>,
+                            tournament_k: usize,
+                            rng: &mut R,
+                            threshold: f64,
+                            coeff: &CompatibilityCoefficients)
+        where R: Rng
+    {
+        assert!(elite_percentage.0 <= selection_percentage.0); // XXX
+        assert!(self.len() > 0);
+        let niches = self.partition(rng, threshold, coeff);
+        assert!(niches.len() > 0);
+        let total_mean: Fitness = niches.iter().map(|n| n.mean_fitness()).sum();
+        // XXX: total_mean = 0.0?
+
+        let mut new_unrated_population = Vec::new();
+        let mut new_rated_population = Vec::new();
+
+        for mut niche in niches.into_iter() {
+            // calculate new size of niche, and size of elites, and selection size.
+            let percentage_of_population: f64 = niche.mean_fitness() / total_mean;
+            assert!(percentage_of_population >= 0.0 && percentage_of_population <= 1.0);
+
+            let niche_size = pop_size as f64 * percentage_of_population;
+
+            // number of elitary individuals to copy from the old niche generation into the new.
+            let elite_size = (niche_size * elite_percentage.0).round() as usize;
+
+            // number of offspring to produce.
+            let offspring_size = (niche_size * (1.0 - elite_percentage.0)).round() as usize;
+
+            // number of the best individuals to use for mating.
+            let select_size = (niche_size * selection_percentage.0).round() as usize;
+
+            let sorted_niche = niche.sort();
+
+            // at first produce ```offspring_size``` individuals from the top ```select_size```
+            // individuals.
+            if select_size > 0 {
+                let mut n = offspring_size;
+                while n > 0 {
+                    let parent1 = selection::tournament_selection_fast(rng,
+                                                                       |i, j| {
+                                                                           sorted_niche.compare_ij(i,j)
+                                                                       },
+                                                                       select_size,
+                                                                       tournament_k);
+                    let parent2 = selection::tournament_selection_fast(rng,
+                                                                       |i, j| {
+                                                                           sorted_niche.compare_ij(i,j)
+                                                                       },
+                                                                       select_size,
+                                                                       tournament_k);
+                    let offspring = sorted_niche.genomes[parent1].1.clone();
+                    // XXX: mate(parent1, parent2)
+                    new_unrated_population.push(offspring);
+                    n -= 1;
+                }
+            }
+
+            // now copy the elites
+            new_rated_population.extend(sorted_niche.genomes.into_iter().take(elite_size));
+        }
+    }
 
     // Partitions the whole population into species (niches)
-    fn partition<R: Rng>(self,
-                         rng: &mut R,
-                         threshold: f64,
-                         coeff: &CompatibilityCoefficients)
-                         -> Vec<Niche> {
+    fn partition<R>(self,
+                    rng: &mut R,
+                    threshold: f64,
+                    coeff: &CompatibilityCoefficients)
+                    -> Vec<Niche>
+        where R: Rng
+    {
         let mut niches: Vec<Niche> = Vec::new();
 
         'outer: for (fitness, genome) in self.genomes.into_iter() {
@@ -269,7 +370,7 @@ impl Population {
                     Some(&(_, ref probe)) => {
                         compatibility(&probe.link_genes, &genome.link_genes, coeff) < threshold
                     }
-                    // If a niche is empty, a genome always is compatible
+                    // If a niche is empty, a genome always is compatible (note that a niche can't be empyt)
                     None => true,
                 };
                 if compatible {
