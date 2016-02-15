c3c78f2e25a4164f258091829edfbd6d237a8c53
Michael Neumann <mneumann@ntecs.de>
2016-02-15 13:24:54 +0100

    Implement Compositional Pattern Producing Networks (CPPN)

--- examples/net.rs
+++ examples/net.rs
@@ -4,10 +4,10 @@ extern crate graph_neighbor_matching;
 extern crate graph_io_gml as gml;
 extern crate closed01;
 extern crate petgraph;
+extern crate cppn;
 
 use neat::population::{Population, Unrated, Runner};
-use neat::network::{NetworkGenome, NetworkGenomeDistance, NodeType, Environment,
-                    ElementStrategy};
+use neat::network::{NetworkGenome, NetworkGenomeDistance, NodeType, Environment, ElementStrategy};
 use neat::fitness::Fitness;
 use neat::traits::Mate;
 use neat::crossover::ProbabilisticCrossover;
@@ -18,7 +18,10 @@ use rand::{Rng, Closed01};
 use std::marker::PhantomData;
 use neat::mutate::{MutateMethod, MutateMethodWeighting};
 use petgraph::Graph as PetGraph;
-use petgraph::Directed;
+use petgraph::{Directed, EdgeDirection};
+use petgraph::graph::NodeIndex;
+use std::collections::BTreeMap;
+use cppn::bipolar::{ActivationFunction, Linear, Gaussian, Sigmoid, Sine};
 
 fn load_graph(graph_file: &str) -> OwnedGraph<NodeType> {
     use std::fs::File;
@@ -38,7 +41,9 @@ fn load_graph(graph_file: &str) -> OwnedGraph<NodeType> {
                                            match s {
                                                "input" => NodeType::Input(0), // XXX
                                                "output" => NodeType::Output(0), // XXX
-                                               "hidden" => NodeType::Hidden{activation_function: 0}, // XXX
+                                               "hidden" => {
+                                                   NodeType::Hidden { activation_function: 0 }
+                                               } // XXX
                                                _ => panic!("Invalid node type/weight"),
                                            }
                                        })
@@ -46,7 +51,7 @@ fn load_graph(graph_file: &str) -> OwnedGraph<NodeType> {
                                },
                                &|_| -> Option<()> { Some(()) })
                     .unwrap();
-    OwnedGraph::from_petgraph(&graph, NodeType::Hidden{activation_function: 0})
+    OwnedGraph::from_petgraph(&graph, NodeType::Hidden { activation_function: 0 })
 }
 
 fn genome_to_graph(genome: &NetworkGenome) -> OwnedGraph<NodeType> {
@@ -59,19 +64,176 @@ fn genome_to_graph(genome: &NetworkGenome) -> OwnedGraph<NodeType> {
 
     for link in genome.link_genes.map.values() {
         if link.active {
-            builder.add_edge(link.source_node_gene.get(), link.target_node_gene.get(), closed01::Closed01::new(link.weight as f32));
+            builder.add_edge(link.source_node_gene.get(),
+                             link.target_node_gene.get(),
+                             closed01::Closed01::new(link.weight as f32));
         }
     }
 
     return builder.graph();
 }
 
+struct CppnNode {
+    value: Option<f64>,
+    node_type: NodeType,
+}
+
+struct CppnGraph {
+    graph: PetGraph<CppnNode, f64, Directed>,
+}
+
+// A Compositional Pattern Producing Network (CPPN)
+struct Cppn {
+    graph: CppnGraph,
+    inputs: BTreeMap<u32, NodeIndex>,
+    outputs: BTreeMap<u32, NodeIndex>,
+}
+
+impl CppnGraph {
+    fn eval_activation_function(activation_function: u32, input: f64) -> f64 {
+        match activation_function {
+            0 => {
+                // Null
+                input
+            }
+            1 => Linear::calculate(input).get(),
+            2 => Gaussian::calculate(input).get(),
+            3 => Sigmoid::calculate(input).get(),
+            4 => Sine::calculate(input).get(),
+            _ => {
+                panic!("invalid activation function");
+            }
+        }
+    }
+
+    /// Reset all cached values to None.
+    fn reset(&mut self) {
+        for idx in self.graph.node_indices() {
+            self.graph.node_weight_mut(idx).unwrap().value = None;
+        }
+    }
+
+    fn get_node_value(&self, idx: NodeIndex) -> f64 {
+        self.graph.node_weight(idx).unwrap().value.unwrap()
+    }
+
+    fn set_node_value(&mut self, idx: NodeIndex, value: f64) {
+        self.graph.node_weight_mut(idx).unwrap().value = Some(value);
+    }
+
+    fn sum_incoming_links(&mut self, idx: NodeIndex) -> f64 {
+        let mut sum: f64 = 0.0;
+
+        // evaluate all incoming nodes
+        let mut walker = self.graph.neighbors_directed(idx, EdgeDirection::Incoming).detach();
+        while let Some((edge_idx, node_idx)) = walker.next(&self.graph) {
+            let value = self.eval_node(node_idx);
+            let weight = self.graph.edge_weight(edge_idx).unwrap();
+
+            sum += weight * value;
+        }
+
+        return sum;
+    }
+
+    /// Recursively evaluate this node and all dependent nodes.
+    fn eval_node(&mut self, idx: NodeIndex) -> f64 {
+        // check if we have a cached value, and return it
+        if let Some(w) = self.graph.node_weight(idx).unwrap().value {
+            return w;
+        }
+
+        // check again
+        let new_value = match self.graph.node_weight(idx).unwrap().node_type {
+            NodeType::Input(..) => {
+                // There must be a value for an input.
+                panic!();
+            }
+            NodeType::Output(n) => {
+                // An output node simply sums up all incoming links.
+                // there is no activation function applied.
+                self.sum_incoming_links(idx)
+            }
+            NodeType::Hidden{activation_function} => {
+                let sum = self.sum_incoming_links(idx);
+                // apply the activation function to the incoming signal.
+                CppnGraph::eval_activation_function(activation_function, sum)
+            }
+        };
+
+        // update the cache value.
+        self.graph.node_weight_mut(idx).unwrap().value = Some(new_value);
+
+        return new_value;
+    }
+}
+
+impl Cppn {
+    /// Read the `n`-th output value.
+    /// Panics if the value has not been calculated before or is out of bounds.
+    pub fn read_output(&self, n: u32) -> f64 {
+        self.graph.get_node_value(self.outputs[&n])
+    }
+
+    /// Set the `n`-th input to `value`
+    fn set_input(&mut self, n: u32, value: f64) {
+        self.graph.set_node_value(self.inputs[&n], value);
+    }
+
+    pub fn evaluate(&mut self, inputs: &[f64]) -> Vec<(u32, f64)> {
+        assert!(inputs.len() == self.inputs.len());
+        self.graph.reset();
+        for (i, &input) in inputs.iter().enumerate() {
+            self.set_input(i as u32, input);
+        }
+
+        let mut output_values = Vec::new();
+        // calculate new values for each output node
+        for (&i, &output_node_idx) in self.outputs.iter() {
+            output_values.push((i, self.graph.eval_node(output_node_idx)));
+        }
+
+        output_values
+    }
+}
+
 // Treats the graph as CPPN and constructs a graph.
-// For the output node (there should be only one!), recursively build up a function
-fn genome_to_graph_hyperneat(genome: &NetworkGenome) -> OwnedGraph<NodeType> {
+fn genome_to_cppn(genome: &NetworkGenome) -> Cppn {
     let graph = genome_to_graph(genome);
     let petgraph = graph.to_petgraph();
-    graph
+    let petgraph = petgraph.map(|_ni, n| {
+                                    CppnNode {
+                                        value: None,
+                                        node_type: n.clone(),
+                                    }
+                                },
+                                |_ei, e| e.get() as f64);
+
+    // make sure that the graph does not contain a cycle.
+    assert!(!petgraph::algo::is_cyclic_directed(&petgraph));
+
+    let mut inputs = BTreeMap::new();
+    let mut outputs = BTreeMap::new();
+
+    for idx in petgraph.node_indices() {
+        match petgraph.node_weight(idx).unwrap() {
+            &CppnNode{node_type: NodeType::Input(n), ..} => {
+                let old = inputs.insert(n, idx);
+                assert!(old.is_none());
+            }
+            &CppnNode{node_type: NodeType::Output(n), ..} => {
+                let old = outputs.insert(n, idx);
+                assert!(old.is_none());
+            }
+            &CppnNode{node_type: NodeType::Hidden{..}, ..} => {}
+        }
+    }
+
+    Cppn {
+        graph: CppnGraph { graph: petgraph },
+        inputs: inputs,
+        outputs: outputs,
+    }
 }
 
 #[derive(Debug)]
@@ -88,7 +250,7 @@ impl NodeColorMatching<NodeType> for NodeColors {
                            node_j_value: &NodeType)
                            -> closed01::Closed01<f32> {
 
-        // Treat Hidden nodes as equal regardless of their activation function.
+        // Treat nodes as equal regardless of their activation function or input/output number.
         let eq = match (node_i_value, node_j_value) {
             (&NodeType::Input(..), &NodeType::Input(..)) => true,
             (&NodeType::Output(..), &NodeType::Output(..)) => true,
@@ -152,9 +314,10 @@ impl<'a, T: ElementStrategy> Mate<NetworkGenome> for Mater<'a, T> {
         } else {
             // mutate
             let mutate_method = MutateMethod::random_with(&self.mutate_weights, rng);
-            self.env.mutate(parent_left, mutate_method, rng).
-                or_else(|| self.env.mutate(parent_right, mutate_method, rng)).
-                unwrap_or_else(|| parent_left.clone())
+            self.env
+                .mutate(parent_left, mutate_method, rng)
+                .or_else(|| self.env.mutate(parent_right, mutate_method, rng))
+                .unwrap_or_else(|| parent_left.clone())
         }
     }
 }
@@ -186,12 +349,12 @@ fn main() {
     let mut mater = Mater {
         p_crossover: Closed01(0.5),
         p_crossover_detail: ProbabilisticCrossover {
-                prob_match_left: Closed01(0.5), /* NEAT always selects a random parent for matching genes */
-                prob_disjoint_left: Closed01(0.9),
-                prob_excess_left: Closed01(0.9),
-                prob_disjoint_right: Closed01(0.15),
-                prob_excess_right: Closed01(0.15),
-            },
+            prob_match_left: Closed01(0.5), /* NEAT always selects a random parent for matching genes */
+            prob_disjoint_left: Closed01(0.9),
+            prob_excess_left: Closed01(0.9),
+            prob_disjoint_right: Closed01(0.15),
+            prob_excess_right: Closed01(0.15),
+        },
         mutate_weights: MutateMethodWeighting {
             w_modify_weight: 1,
             w_add_node: 1,
