0aee0a60e2aeb292743a2d1bff7d882ecb42687f
Michael Neumann <mneumann@ntecs.de>
2016-02-23 12:44:30 +0100

    Refactor alignment

--- src/alignment.rs
+++ src/alignment.rs
@@ -1,80 +1,81 @@
-use sorted_vec::SortedUniqueVec;
+use std::iter::Peekable;
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Alignment<'a, T: 'a> {
-    Match(&'a T, &'a T),
-    ExcessLeftHead(&'a T),
-    ExcessRightHead(&'a T),
-    ExcessLeftTail(&'a T),
-    ExcessRightTail(&'a T),
-    DisjointLeft(&'a T),
-    DisjointRight(&'a T),
+pub enum Alignment<T> {
+    Match(T, T),
+    ExcessLeftHead(T),
+    ExcessRightHead(T),
+    ExcessLeftTail(T),
+    ExcessRightTail(T),
+    DisjointLeft(T),
+    DisjointRight(T),
 }
 
-impl<'a, T: 'a> Alignment<'a, T> {
+impl<T> Alignment<T> {
     pub fn is_match(&self) -> bool {
-        match self {
-            &Alignment::Match(..) => true,
+        match *self {
+            Alignment::Match(..) => true,
             _ => false,
         }
     }
 
     pub fn is_disjoint_left(&self) -> bool {
-        match self {
-            &Alignment::DisjointLeft(..) => true,
+        match *self {
+            Alignment::DisjointLeft(..) => true,
             _ => false,
         }
     }
 
     pub fn is_disjoint_right(&self) -> bool {
-        match self {
-            &Alignment::DisjointRight(..) => true,
+        match *self {
+            Alignment::DisjointRight(..) => true,
             _ => false,
         }
     }
 
     pub fn is_disjoint(&self) -> bool {
-        match self {
-            &Alignment::DisjointLeft(..) |
-            &Alignment::DisjointRight(..) => true,
+        match *self {
+            Alignment::DisjointLeft(..) |
+            Alignment::DisjointRight(..) => true,
             _ => false,
         }
     }
 
     pub fn is_excess_left(&self) -> bool {
-        match self {
-            &Alignment::ExcessLeftHead(..) |
-            &Alignment::ExcessLeftTail(..) => true,
+        match *self {
+            Alignment::ExcessLeftHead(..) |
+            Alignment::ExcessLeftTail(..) => true,
             _ => false,
         }
     }
 
     pub fn is_excess_right(&self) -> bool {
-        match self {
-            &Alignment::ExcessRightHead(..) |
-            &Alignment::ExcessRightTail(..) => true,
+        match *self {
+            Alignment::ExcessRightHead(..) |
+            Alignment::ExcessRightTail(..) => true,
             _ => false,
         }
     }
 
     pub fn is_excess(&self) -> bool {
-        match self {
-            &Alignment::ExcessLeftHead(..) |
-            &Alignment::ExcessLeftTail(..) |
-            &Alignment::ExcessRightHead(..) |
-            &Alignment::ExcessRightTail(..) => true,
+        match *self {
+            Alignment::ExcessLeftHead(..) |
+            Alignment::ExcessLeftTail(..) |
+            Alignment::ExcessRightHead(..) |
+            Alignment::ExcessRightTail(..) => true,
             _ => false,
         }
     }
 }
 
-/// Align the items of two sorted unique vectors.
-pub fn align<'a, F, T>(a: &'a SortedUniqueVec<T>, b: &'a SortedUniqueVec<T>, f: &mut F)
-    where F: FnMut(Alignment<'a, T>),
-          T: Ord + Clone
+/// Align the items of two sorted (unique) iterators.
+pub fn align_sorted_iterators<F, I>(a: I, b: I, mut f: F)
+    where F: FnMut(Alignment<I::Item>),
+          I::Item: Ord + Clone,
+          I: Iterator,
 {
-    let mut left_iter = a.as_ref().iter().peekable();
-    let mut right_iter = b.as_ref().iter().peekable();
+    let mut left_iter = a.peekable();
+    let mut right_iter = b.peekable();
     let mut left_count = 0;
     let mut right_count = 0;
 
@@ -90,7 +91,7 @@ pub fn align<'a, F, T>(a: &'a SortedUniqueVec<T>, b: &'a SortedUniqueVec<T>, f: 
         let take;
 
         match (left_iter.peek(), right_iter.peek()) {
-            (Some(l), Some(r)) => {
+            (Some(ref l), Some(ref r)) => {
                 if l < r {
                     take = Take::OneLeft;
                 } else if r < l {
@@ -137,7 +138,7 @@ pub fn align<'a, F, T>(a: &'a SortedUniqueVec<T>, b: &'a SortedUniqueVec<T>, f: 
                 // two equal values
                 let left_value = left_iter.next().unwrap();
                 let right_value = right_iter.next().unwrap();
-                debug_assert!(left_value.eq(right_value));
+                debug_assert!(left_value.eq(&right_value));
 
                 f(Alignment::Match(left_value, right_value));
 
@@ -163,75 +164,75 @@ pub fn align<'a, F, T>(a: &'a SortedUniqueVec<T>, b: &'a SortedUniqueVec<T>, f: 
 }
 
 #[cfg(test)]
-fn align_as_vec<'a, T>(a: &'a SortedUniqueVec<T>,
-                       b: &'a SortedUniqueVec<T>)
-                       -> Vec<Alignment<'a, T>>
-    where T: Ord + Clone
-{
-    let mut c = Vec::new();
-    align(a, b, &mut |alignment| c.push(alignment));
-    c
-}
+mod tests {
+    use std::collections::BTreeSet;
+    use super::{Alignment, align_sorted_iterators};
 
-#[test]
-fn test_align() {
-    use sorted_vec::is_sorted_unique;
+    fn align_as_vec<I>(a: I, b: I) -> Vec<Alignment<I::Item>>
+        where I::Item: Ord + Clone,
+              I: Iterator,
+    {
+        let mut c = Vec::new();
+        align_sorted_iterators(a, b, |alignment| c.push(alignment));
+        c
+    }
 
-    let mut s1 = SortedUniqueVec::new();
-    s1.insert(0);
-    s1.insert(1);
-    s1.insert(5);
-    s1.insert(8);
-    assert!(is_sorted_unique(&s1));
+    #[test]
+    fn test_align_sorted_iterators() {
+        let mut s1 = BTreeSet::<usize>::new();
+        s1.insert(0);
+        s1.insert(1);
+        s1.insert(5);
+        s1.insert(8);
 
-    let mut s2 = SortedUniqueVec::new();
-    s2.insert(1);
-    s2.insert(5);
-    s2.insert(7);
-    s2.insert(9);
-    s2.insert(55);
-    assert!(is_sorted_unique(&s2));
+        let mut s2 = BTreeSet::<usize>::new();
+        s2.insert(1);
+        s2.insert(5);
+        s2.insert(7);
+        s2.insert(9);
+        s2.insert(55);
 
-    let mut r = SortedUniqueVec::new();
-    align(&s1,
-          &s2,
-          &mut |alignment| {
-              match alignment {
-                  Alignment::Match(a, _b) => r.push((*a).clone()),
-                  Alignment::ExcessLeftHead(a) |
-                  Alignment::ExcessRightHead(a) |
-                  Alignment::ExcessLeftTail(a) |
-                  Alignment::ExcessRightTail(a) |
-                  Alignment::DisjointLeft(a) |
-                  Alignment::DisjointRight(a) => r.push((*a).clone()),
-              }
-          });
+        let mut r = Vec::new();
+        super::align_sorted_iterators(s1.iter().cloned(),
+        s2.iter().cloned(),
+        |alignment| {
+            match alignment {
+                Alignment::Match(a, _b) => r.push(a),
+                Alignment::ExcessLeftHead(a) |
+                    Alignment::ExcessRightHead(a) |
+                    Alignment::ExcessLeftTail(a) |
+                    Alignment::ExcessRightTail(a) |
+                    Alignment::DisjointLeft(a) |
+                    Alignment::DisjointRight(a) => r.push(a),
+            }
+        });
 
-    assert!(is_sorted_unique(&r));
-    assert_eq!(&[0, 1, 5, 7, 8, 9, 55][..], r.as_ref());
-}
+        assert_eq!(vec![0, 1, 5, 7, 8, 9, 55], r);
+    }
 
-#[test]
-fn test_align_as_vec() {
-    let mut left = SortedUniqueVec::new();
-    let mut right = SortedUniqueVec::new();
-    // 40, 46, 50
-    left.insert(50);
-    left.insert(46);
-    left.insert(40);
+    #[test]
+    fn test_align_as_vec() {
+        let mut left = BTreeSet::<usize>::new();
+        let mut right = BTreeSet::<usize>::new();
 
-    // 45, 50, 51, 52
-    right.insert(50);
-    right.insert(45);
-    right.insert(51);
-    right.insert(52);
+        // 40, 46, 50
+        left.insert(50);
+        left.insert(46);
+        left.insert(40);
 
-    let c = align_as_vec(&left, &right);
-    assert_eq!(6, c.len());
-    assert_eq!(Alignment::ExcessLeftHead(&40), c[0]);
-    assert_eq!(Alignment::DisjointRight(&45), c[1]);
-    assert_eq!(Alignment::DisjointLeft(&46), c[2]);
-    assert_eq!(Alignment::Match(&50, &50), c[3]);
-    assert_eq!(Alignment::ExcessRightTail(&51), c[4]);
-    assert_eq!(Alignment::ExcessRightTail(&52), c[5]);
+        // 45, 50, 51, 52
+        right.insert(50);
+        right.insert(45);
+        right.insert(51);
+        right.insert(52);
+
+        let c = align_as_vec(left.iter().cloned(), right.iter().cloned());
+        assert_eq!(6, c.len());
+        assert_eq!(Alignment::ExcessLeftHead(40), c[0]);
+        assert_eq!(Alignment::DisjointRight(45), c[1]);
+        assert_eq!(Alignment::DisjointLeft(46), c[2]);
+        assert_eq!(Alignment::Match(50, 50), c[3]);
+        assert_eq!(Alignment::ExcessRightTail(51), c[4]);
+        assert_eq!(Alignment::ExcessRightTail(52), c[5]);
+    }
 }
